c3fdc3af2b19719bbc8db391f9a43f7964aaaee5
littleEndianISHorrible <123942486+littleEndianISHorrible@users.noreply.github.com>
2025-06-30 17:15:55 +0800

    Add files via upload

---
+++ logicalpositional.h
@@ -0,0 +1,21 @@
+/**************************
+ * Object positioning generation (backrooms)
+ *
+ **************************/
+#include "main_includes.h"
+//list of dis[ossable assets
+
+//imported from other files
+
+// algorthim for determining what they game wants
+//generating a fingerprint that the game scene will follow using regular coordinates(walls)
+//then generate a hash map of all these (single vector points)
+
+void algorthim(){
+}
+
+char* dissposableassets(){
+	return 0;
+}
+
+
---
+++ main_includes.h
@@ -0,0 +1,24 @@
+//main_includes.h
+#ifndef MAIN_INCLUDES_H
+#define MAIN_INCLUDES_H
+
+#include <windows.h>
+#include <gl/gl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <io.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include "physics.h"
+#include "userinput.h"
+#include "logicalpositional.h"
+#include "engine.h"
+
+#include <stdbool.h>
+#endif // MAIN_INCLUDES_H
+
+
+
+
